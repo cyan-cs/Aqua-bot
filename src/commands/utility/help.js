7e1361b5a860bd64fb6b62656efb7df6aaf24c69
cyan-Dev <akatsukisan1212@gmail.com>
2025-07-31 00:19:23 +0900

    Update help.js

--- src/commands/utility/help.js
+++ src/commands/utility/help.js
@@ -6,12 +6,12 @@ module.exports = {
         .setDescription('ヘルプを表示します。'),
     async executeSlash(interaction) {
         const readmeUrl = 'https://github.com/cyan-cs/DiscordBOT-cs';
-        const supportServerUrl = 'https://discord.gg/g5jur2YHc3';
+        const supportServerUrl = 'https://discord.gg/nq2tQXHAXt';
 
         const embed = new EmbedBuilder()
             .setColor(0x0099ff)
-            .setTitle('mms-help')
-            .setDescription(`コマンド一覧: [GitHub](${readmeUrl}) \n サポート鯖: [しあん鯖](${supportServerUrl})`)
+            .setTitle('まかろに-help')
+            .setDescription(`コマンド一覧: [GitHub](${readmeUrl}) \n サポート鯖: [ぱすた](${supportServerUrl})`)
             .setFooter({
                 text: `実行者: ${interaction.user.tag}`,
                 iconURL: interaction.user.displayAvatarURL({ dynamic: true })
